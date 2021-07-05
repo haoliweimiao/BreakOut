b7a1b5fc4088e907a879e307bc82deab4469140a
haoliwei <haoliweiwork@gmail.com>
2021-07-05 23:43:51 +0800

    Version: 0.1.6 [+]增加碰撞处理

--- src/game.cpp
+++ src/game.cpp
@@ -9,23 +9,12 @@
 #include "game.h"
 #include "resource_manager.h"
 #include "sprite_renderer.h"
+#include "game_object.h"
 #include "ball_object.h"
 
 // Game-related State data
 SpriteRenderer *Renderer;
-
-// 初始化挡板的大小
-const glm::vec2 PLAYER_SIZE(100, 20);
-// 初始化当班的速率
-const GLfloat PLAYER_VELOCITY(500.0f);
-
 GameObject *Player;
-
-// 初始化球的速度
-const glm::vec2 INITIAL_BALL_VELOCITY(100.0f, -350.0f);
-// 球的半径
-const GLfloat BALL_RADIUS = 12.5f;
-
 BallObject *Ball;
 
 Game::Game(GLuint width, GLuint height)
@@ -35,6 +24,9 @@ Game::Game(GLuint width, GLuint height)
 
 Game::~Game()
 {
+    delete Renderer;
+    delete Player;
+    delete Ball;
 }
 
 void Game::Init()
@@ -86,10 +78,16 @@ void Game::Init()
 
 void Game::Update(GLfloat dt)
 {
-    // 更新对象
+    // Update objects
     Ball->Move(dt, this->Width);
-    // 检测碰撞
+    // Check for collisions
     this->DoCollisions();
+    // Check loss condition
+    if (Ball->Position.y >= this->Height) // Did ball reach bottom edge?
+    {
+        this->ResetLevel();
+        this->ResetPlayer();
+    }
 }
 
 void Game::ProcessInput(GLfloat dt)
@@ -97,7 +95,7 @@ void Game::ProcessInput(GLfloat dt)
     if (this->State == GAME_ACTIVE)
     {
         GLfloat velocity = PLAYER_VELOCITY * dt;
-        // 移动玩家挡板
+        // Move playerboard
         if (this->Keys[GLFW_KEY_A])
         {
             if (Player->Position.x >= 0)
@@ -123,54 +121,43 @@ void Game::ProcessInput(GLfloat dt)
 
 void Game::Render()
 {
-    // Texture2D texture = ResourceManager::GetTexture(TEXTURE_NAME_FACE);
-    // Renderer->DrawSprite(texture, glm::vec2(200, 200), glm::vec2(300, 400), 45.0f, glm::vec3(0.0f, 1.0f, 0.0f));
-
     if (this->State == GAME_ACTIVE)
     {
-        // 绘制背景
-        Renderer->DrawSprite(ResourceManager::GetTexture(TEXTURE_NAME_BACKGROUND),
-                             glm::vec2(0, 0), glm::vec2(this->Width, this->Height), 0.0f);
-        // 绘制关卡
+        // Draw background
+        Renderer->DrawSprite(ResourceManager::GetTexture("background"), glm::vec2(0, 0), glm::vec2(this->Width, this->Height), 0.0f);
+        // Draw level
         this->Levels[this->Level].Draw(*Renderer);
-
-        // 绘制挡板
+        // Draw player
         Player->Draw(*Renderer);
-
-        // 绘制球
+        // Draw ball
         Ball->Draw(*Renderer);
     }
 }
 
-GLboolean CheckCollision(BallObject &one, GameObject &two)
+void Game::ResetLevel()
 {
-    // GameObject &one, GameObject &two
-    // // AABB - AABB collision
-    // // x轴方向碰撞？
-    // bool collisionX = one.Position.x + one.Size.x >= two.Position.x &&
-    //                   two.Position.x + two.Size.x >= one.Position.x;
-    // // y轴方向碰撞？
-    // bool collisionY = one.Position.y + one.Size.y >= two.Position.y &&
-    //                   two.Position.y + two.Size.y >= one.Position.y;
-    // // 只有两个轴向都有碰撞时才碰撞
-    // return collisionX && collisionY;
-
-    // 获取圆的中心
-    glm::vec2 center(one.Position + one.Radius);
-    // 计算AABB的信息（中心、半边长）
-    glm::vec2 aabb_half_extents(two.Size.x / 2, two.Size.y / 2);
-    glm::vec2 aabb_center(
-        two.Position.x + aabb_half_extents.x,
-        two.Position.y + aabb_half_extents.y);
-    // 获取两个中心的差矢量
-    glm::vec2 difference = center - aabb_center;
-    glm::vec2 clamped = glm::clamp(difference, -aabb_half_extents, aabb_half_extents);
-    // AABB_center加上clamped这样就得到了碰撞箱上距离圆最近的点closest
-    glm::vec2 closest = aabb_center + clamped;
-    // 获得圆心center和最近点closest的矢量并判断是否 length <= radius
-    difference = closest - center;
-    return glm::length(difference) < one.Radius;
+    if (this->Level == 0)
+        this->Levels[0].Load("levels/one.lvl", this->Width, this->Height * 0.5f);
+    else if (this->Level == 1)
+        this->Levels[1].Load("levels/two.lvl", this->Width, this->Height * 0.5f);
+    else if (this->Level == 2)
+        this->Levels[2].Load("levels/three.lvl", this->Width, this->Height * 0.5f);
+    else if (this->Level == 3)
+        this->Levels[3].Load("levels/four.lvl", this->Width, this->Height * 0.5f);
 }
+
+void Game::ResetPlayer()
+{
+    // Reset player/ball stats
+    Player->Size = PLAYER_SIZE;
+    Player->Position = glm::vec2(this->Width / 2 - PLAYER_SIZE.x / 2, this->Height - PLAYER_SIZE.y);
+    Ball->Reset(Player->Position + glm::vec2(PLAYER_SIZE.x / 2 - BALL_RADIUS, -(BALL_RADIUS * 2)), INITIAL_BALL_VELOCITY);
+}
+
+// Collision detection
+GLboolean CheckCollision(GameObject &one, GameObject &two);
+Collision CheckCollision(BallObject &one, GameObject &two);
+Direction VectorDirection(glm::vec2 closest);
 
 void Game::DoCollisions()
 {
@@ -178,11 +165,109 @@ void Game::DoCollisions()
     {
         if (!box.Destroyed)
         {
-            if (CheckCollision(*Ball, box))
+            Collision collision = CheckCollision(*Ball, box);
+            if (std::get<0>(collision)) // If collision is true
             {
+                // Destroy block if not solid
                 if (!box.IsSolid)
                     box.Destroyed = GL_TRUE;
+                // Collision resolution
+                Direction dir = std::get<1>(collision);
+                glm::vec2 diff_vector = std::get<2>(collision);
+                if (dir == LEFT || dir == RIGHT) // Horizontal collision
+                {
+                    Ball->Velocity.x = -Ball->Velocity.x; // Reverse horizontal velocity
+                    // Relocate
+                    GLfloat penetration = Ball->Radius - std::abs(diff_vector.x);
+                    if (dir == LEFT)
+                        Ball->Position.x += penetration; // Move ball to right
+                    else
+                        Ball->Position.x -= penetration; // Move ball to left;
+                }
+                else // Vertical collision
+                {
+                    Ball->Velocity.y = -Ball->Velocity.y; // Reverse vertical velocity
+                    // Relocate
+                    GLfloat penetration = Ball->Radius - std::abs(diff_vector.y);
+                    if (dir == UP)
+                        Ball->Position.y -= penetration; // Move ball bback up
+                    else
+                        Ball->Position.y += penetration; // Move ball back down
+                }
             }
         }
     }
+    // Also check collisions for player pad (unless stuck)
+    Collision result = CheckCollision(*Ball, *Player);
+    if (!Ball->Stuck && std::get<0>(result))
+    {
+        // Check where it hit the board, and change velocity based on where it hit the board
+        GLfloat centerBoard = Player->Position.x + Player->Size.x / 2;
+        GLfloat distance = (Ball->Position.x + Ball->Radius) - centerBoard;
+        GLfloat percentage = distance / (Player->Size.x / 2);
+        // Then move accordingly
+        GLfloat strength = 2.0f;
+        glm::vec2 oldVelocity = Ball->Velocity;
+        Ball->Velocity.x = INITIAL_BALL_VELOCITY.x * percentage * strength;
+        //Ball->Velocity.y = -Ball->Velocity.y;
+        Ball->Velocity = glm::normalize(Ball->Velocity) * glm::length(oldVelocity); // Keep speed consistent over both axes (multiply by length of old velocity, so total strength is not changed)
+        // Fix sticky paddle
+        Ball->Velocity.y = -1 * abs(Ball->Velocity.y);
+    }
+}
+
+GLboolean CheckCollision(GameObject &one, GameObject &two) // AABB - AABB collision
+{
+    // Collision x-axis?
+    bool collisionX = one.Position.x + one.Size.x >= two.Position.x &&
+                      two.Position.x + two.Size.x >= one.Position.x;
+    // Collision y-axis?
+    bool collisionY = one.Position.y + one.Size.y >= two.Position.y &&
+                      two.Position.y + two.Size.y >= one.Position.y;
+    // Collision only if on both axes
+    return collisionX && collisionY;
+}
+
+Collision CheckCollision(BallObject &one, GameObject &two) // AABB - Circle collision
+{
+    // Get center point circle first
+    glm::vec2 center(one.Position + one.Radius);
+    // Calculate AABB info (center, half-extents)
+    glm::vec2 aabb_half_extents(two.Size.x / 2, two.Size.y / 2);
+    glm::vec2 aabb_center(two.Position.x + aabb_half_extents.x, two.Position.y + aabb_half_extents.y);
+    // Get difference vector between both centers
+    glm::vec2 difference = center - aabb_center;
+    glm::vec2 clamped = glm::clamp(difference, -aabb_half_extents, aabb_half_extents);
+    // Now that we know the the clamped values, add this to AABB_center and we get the value of box closest to circle
+    glm::vec2 closest = aabb_center + clamped;
+    // Now retrieve vector between center circle and closest point AABB and check if length < radius
+    difference = closest - center;
+
+    if (glm::length(difference) < one.Radius) // not <= since in that case a collision also occurs when object one exactly touches object two, which they are at the end of each collision resolution stage.
+        return std::make_tuple(GL_TRUE, VectorDirection(difference), difference);
+    else
+        return std::make_tuple(GL_FALSE, UP, glm::vec2(0, 0));
+}
+
+// Calculates which direction a vector is facing (N,E,S or W)
+Direction VectorDirection(glm::vec2 target)
+{
+    glm::vec2 compass[] = {
+        glm::vec2(0.0f, 1.0f),  // up
+        glm::vec2(1.0f, 0.0f),  // right
+        glm::vec2(0.0f, -1.0f), // down
+        glm::vec2(-1.0f, 0.0f)  // left
+    };
+    GLfloat max = 0.0f;
+    GLuint best_match = -1;
+    for (GLuint i = 0; i < 4; i++)
+    {
+        GLfloat dot_product = glm::dot(glm::normalize(target), compass[i]);
+        if (dot_product > max)
+        {
+            max = dot_product;
+            best_match = i;
+        }
+    }
+    return (Direction)best_match;
 }
--- src/game.h
+++ src/game.h
@@ -1,57 +1,79 @@
-//
-// @description: game class
-// @author: von.wu
-// @date: 2021-07-03 21:48:46
-//
-
+/*******************************************************************
+** This code is part of Breakout.
+**
+** Breakout is free software: you can redistribute it and/or modify
+** it under the terms of the CC BY 4.0 license as published by
+** Creative Commons, either version 4 of the License, or (at your
+** option) any later version.
+******************************************************************/
 #ifndef GAME_H
 #define GAME_H
-// include something
-#include "single_import.h"
-#include "game_level.h"
 #include <vector>
+#include <tuple>
 
+#include "single_import.h"
+#include <GLFW/glfw3.h>
+
+#include "game_level.h"
 #define SHADER_NAME_SPRITE "sprite"
 #define TEXTURE_NAME_FACE "face"
 #define TEXTURE_NAME_BACKGROUND "background"
 #define TEXTURE_NAME_BLOCK "block"
 #define TEXTURE_NAME_BLOCK_SOLID "block_solid"
 
-#ifdef __cplusplus
-extern "C"
+// Represents the current state of the game
+enum GameState
 {
-#endif
+    GAME_ACTIVE,
+    GAME_MENU,
+    GAME_WIN
+};
 
-    // your code
-    // 代表了游戏的当前状态
-    enum GameState
-    {
-        GAME_ACTIVE,
-        GAME_MENU,
-        GAME_WIN
-    };
+// Represents the four possible (collision) directions
+enum Direction
+{
+    UP,
+    RIGHT,
+    DOWN,
+    LEFT
+};
+// Defines a Collision typedef that represents collision data
+typedef std::tuple<GLboolean, Direction, glm::vec2> Collision; // <collision?, what direction?, difference vector center - closest point>
 
-    class Game
-    {
-    public:
-        // 游戏状态
-        GameState State;
-        GLboolean Keys[1024];
-        GLuint Width, Height;
-        std::vector<GameLevel> Levels;
-        GLuint Level;
-        // 构造函数/析构函数
-        Game(GLuint width, GLuint height);
-        ~Game();
-        // 初始化游戏状态（加载所有的着色器/纹理/关卡）
-        void Init();
-        // 游戏循环
-        void ProcessInput(GLfloat dt);
-        void Update(GLfloat dt);
-        void Render();
-        void DoCollisions();
-    };
-#ifdef __cplusplus
-}
+// Initial size of the player paddle
+const glm::vec2 PLAYER_SIZE(100, 20);
+// Initial velocity of the player paddle
+const GLfloat PLAYER_VELOCITY(500.0f);
+// Initial velocity of the Ball
+const glm::vec2 INITIAL_BALL_VELOCITY(100.0f, -350.0f);
+// Radius of the ball object
+const GLfloat BALL_RADIUS = 12.5f;
+
+// Game holds all game-related state and functionality.
+// Combines all game-related data into a single class for
+// easy access to each of the components and manageability.
+class Game
+{
+public:
+    // Game state
+    GameState State;
+    GLboolean Keys[1024];
+    GLuint Width, Height;
+    std::vector<GameLevel> Levels;
+    GLuint Level;
+    // Constructor/Destructor
+    Game(GLuint width, GLuint height);
+    ~Game();
+    // Initialize game state (load all shaders/textures/levels)
+    void Init();
+    // GameLoop
+    void ProcessInput(GLfloat dt);
+    void Update(GLfloat dt);
+    void Render();
+    void DoCollisions();
+    // Reset
+    void ResetLevel();
+    void ResetPlayer();
+};
+
 #endif
-#endif // GAME_H
